b4aceab57d20cb36d8cb9e2796f85ce12e52e658
mikem1957 <mike.mikailov@gmail.com>
2025-05-31 12:31:42 -0400

    Update all_images.json

---
+++ google_analytics/google_tag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-D7T4TXB49V"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-D7T4TXB49V');
+</script>
